77e9eccdb33395610ae8fcc917a2328233443320
iosifcostin <josif.costin@gmail.com>
2019-09-06 03:57:59 +0300

    first deploy

--- src/main/java/iosifcostin/MathArena/config/Beans.java
+++ src/main/java/iosifcostin/MathArena/config/Beans.java
@@ -1,4 +1,6 @@
 package iosifcostin.MathArena.config;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import iosifcostin.MathArena.mathMl.MathMlToPng;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +12,11 @@ public class Beans {
     @Bean
     public MathMlToPng mathMlToPng() {
         return new MathMlToPng();
+    }
+
+    @Bean
+    public AmazonS3 amazonS3 (){
+        return new AmazonS3Client();
     }
 
 
